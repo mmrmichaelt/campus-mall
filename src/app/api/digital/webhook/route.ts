3e449ca2b3c451890deb079c3adafab831979568
Moses Maiko Rangi <moses.maiko00@gmail.com>
2026-09-19 18:11:16 +0300

    Fix digital API library import paths

--- src/app/api/digital/webhook/route.ts
+++ src/app/api/digital/webhook/route.ts
@@ -1,7 +1,7 @@
 import crypto from "crypto";
 import { NextRequest } from "next/server";
 
-import { prisma } from "../../../../src/lib/prisma";
+import { prisma } from "../../../../lib/prisma";
 
 function verifySignature(
   rawBody: string,
