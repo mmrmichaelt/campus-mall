656f345ef8f5b20d616ff9f39d93ade9c3aa40ea
Moses Maiko Rangi <moses.maiko00@gmail.com>
2026-09-19 17:48:50 +0300

    Add global institutions and monetization infrastructure

---
+++ src/data/universities.ts
@@ -0,0 +1,9 @@
+export type Institution = { id?: string; name: string; countryCode: string; city?: string; county?: string; type: string };
+import { universities as kenya } from "./institutions/kenya";
+import { ghanaInstitutions } from "./institutions/ghana";
+import { nigeriaInstitutions } from "./institutions/nigeria";
+import { ugandaInstitutions } from "./institutions/uganda";
+import { rwandaInstitutions } from "./institutions/rwanda";
+import { tanzaniaInstitutions } from "./institutions/tanzania";
+export const universities: Institution[] = [...kenya, ...ghanaInstitutions, ...nigeriaInstitutions, ...ugandaInstitutions, ...rwandaInstitutions, ...tanzaniaInstitutions];
+export function getInstitutions(countryCode: string) { return universities.filter((i) => i.countryCode === countryCode); }
