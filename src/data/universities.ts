73fa26a1c28a38031dfc317ffc20b81ec178a935
Moses Maiko Rangi <moses.maiko00@gmail.com>
2026-09-20 09:19:20 +0300

    Fix empty university options for countries without institution data

--- src/data/universities.ts
+++ src/data/universities.ts
@@ -8,4 +8,19 @@ import { tanzaniaInstitutions } from "./institutions/tanzania";
 export const universities: Institution[] = [...kenya, ...ghanaInstitutions, ...nigeriaInstitutions, ...ugandaInstitutions, ...rwandaInstitutions, ...tanzaniaInstitutions];
 export function getInstitutions(countryCode: string) { return universities.filter((i) => i.countryCode === countryCode); }
 
-export function getUniversitiesByCountry(countryCode: string) { return getInstitutions(countryCode); }
+export function getUniversitiesByCountry(countryCode: string) {
+  const institutions = getInstitutions(countryCode);
+
+  if (institutions.length > 0) {
+    return institutions;
+  }
+
+  return [
+    {
+      id: `${countryCode}-other`,
+      name: "Other university / college",
+      countryCode,
+      type: "University / College",
+    },
+  ];
+}
