b65e6ccd321149a976317c08b96ac2b2fef6dbf3
Moses Maiko Rangi <moses.maiko00@gmail.com>
2026-09-20 08:29:45 +0300

    Fix digital API library imports

--- src/app/api/digital/products/route.ts
+++ src/app/api/digital/products/route.ts
@@ -3,7 +3,7 @@ import {
   DigitalProductType,
 } from "@prisma/client";
 
-import { prisma } from "../../../../src/lib/prisma";
+import { prisma } from "../../../../lib/prisma";
 
 export async function GET(request: NextRequest) {
   try {
