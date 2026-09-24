b4128904be9df855f103096a48865de4817efaf1
Moses Maiko Rangi <moses.maiko00@gmail.com>
2026-09-24 16:01:19 +0300

    Clean listing form imports after institution lock

--- src/components/NewListingForm.tsx
+++ src/components/NewListingForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FormEvent, useEffect, useState } from "react";
+import { FormEvent, useState } from "react";
 import { useRouter } from "next/navigation";
 import { countries } from "@/data/countries";
 
