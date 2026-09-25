07631dcdd82ac69ea6050e08c2c96d85a92b96f0
Moses Maiko Rangi <moses.maiko00@gmail.com>
2026-09-25 17:33:13 +0300

    Fix remaining institution cell regex

--- src/lib/institution-directory.ts
+++ src/lib/institution-directory.ts
@@ -26,7 +26,7 @@ async function getKenyaTvetaInstitutions(): Promise<Institution[]> {
     const html = await response.text();
     const institutions: Institution[] = [];
     const rowRegex = /<tr[^>]*>([\\s\\S]*?)<\/tr>/gi;
-    const cellRegex = /<td[^>]*>([\\s\\S]*?)<\\/td>/gi;
+    const cellRegex = /<td[^>]*>([\s\S]*?)<\/td>/gi;
 
     for (const row of html.matchAll(rowRegex)) {
       const cells = [...row[1].matchAll(cellRegex)].map((match) =>
