39342e56e059c9236b4a20863d0f8eb9c3d2b4cd
Moses Maiko Rangi <moses.maiko00@gmail.com>
2026-09-25 18:28:42 +0300

    Add visible Continue as Guest option

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
 import AccountForms from "../../components/AccountForms";
 
 export const dynamic = "force-dynamic";
 
 export default function JoinPage() {
-  return <AccountForms />;
+  return (
+    <>
+      <AccountForms />
+      <div style={{ textAlign: "center", marginTop: "-8px", paddingBottom: "24px" }}>
+        <Link href="/guest" className="primary-btn">Continue as Guest</Link>
+      </div>
+    </>
+  );
 }
