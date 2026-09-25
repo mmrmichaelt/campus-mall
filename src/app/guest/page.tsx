8fc9b57fa6bc0688874c47c234a90df4a9488558
Moses Maiko Rangi <moses.maiko00@gmail.com>
2026-09-25 17:58:44 +0300

    Add Create Account button to guest page

--- src/app/guest/page.tsx
+++ src/app/guest/page.tsx
@@ -1,5 +1,11 @@
+import Link from "next/link";
 import AccountForms from "@/components/AccountForms";
 
 export default function GuestPage() {
-  return <AccountForms initialMode="guest" />;
+  return <>
+      <AccountForms initialMode="guest" />
+      <div style={{ textAlign: "center", marginTop: "-24px", paddingBottom: "24px" }}>
+        <Link href="/join" className="primary-btn">Create Account</Link>
+      </div>
+    </>;
 }
